ec5a7212549bb1f39deb417f12113eadaa84d8a3
Pulpgrinder <pulpgrinder@protonmail.com>
2021-01-05 19:32:47 -0900

    Updating build

--- dist/sw.js
+++ dist/sw.js
@@ -1,49 +1,43 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v0.95-build_13').then(function(cache) {
+    caches.open('v0.95-329').then(function(cache) {
       return cache.addAll([
-        'fronkensteen.html'
+        'fronkensteen_launcher.html',
+        'apple-touch-icon.png',
+        'icon-192x192.png',
+        'icon-512x512.png',
+        'logo.png',
+        'shareicon.png',
       ]);
     })
   );
 });
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request).then(function(response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
     if (response !== undefined) {
       return response;
     } else {
       return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
         let responseClone = response.clone();
-
-        caches.open('v0.95-build_13').then(function (cache) {
+        caches.open('v0.95-329').then(function (cache) {
           cache.put(event.request, responseClone);
         });
         return response;
       }).catch(function () {
-        return caches.match('/fronkensteen.html');
+        return caches.match(event.request);
       });
     }
   }));
 });
-
-/*
 self.addEventListener("activate", event => {
-  console.log("activate")
   event.waitUntil(
     caches.keys().then (cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== 'v0.95-build_13'){
-            console.log("deleting old cache: " + cacheName)
+          if (cacheName !== 'v0.95-329'){
             return caches.delete(cacheName);
           }
-        });
+        })
       );
     }));
-  }); */
+  });
